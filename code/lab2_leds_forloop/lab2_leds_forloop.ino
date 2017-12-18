ec7ba23be3082d537edbe53c9e06ec61997284b7
Holm Smidt <hsmidt@hawaii.edu>
2017-12-17 15:15:33 -1000

    add for loop code

---
+++ code/lab2_leds_forloop/lab2_leds_forloop.ino
@@ -0,0 +1,47 @@
+/* 
+ *  
+ *  Title: X-Mas Lights, For Loop
+ *  Objective: Control multiple LEDs
+ */
+
+int pin; 
+
+void setup() {
+  // set communication with the computer
+  Serial.begin(9600);
+
+  // set the pins as output pins
+  for (pin=2; pin < 14; pin++) { 
+    pinMode(pin, OUTPUT);    
+  }
+}
+
+void loop() {
+  // blink all twelve at the same time
+
+  // blink on
+  for (pin=2; pin < 14; pin++) { 
+    digitalWrite(pin, HIGH); 
+  }
+
+  // wait
+  delay(100); 
+
+  // blink off
+  for (pin=2; pin < 14; pin++) { 
+    digitalWrite(pin, LOW);
+  }
+  
+  
+  // turn led's on/off in a row with 100 millisecond interval
+  for (pin=2; pin < 14; pin++) { 
+    digitalWrite(pin, HIGH);
+    delay(100); 
+    digitalWrite(pin, LOW);
+  }
+
+  // wow  ... so much less typing 
+  // can you make it go backwards from 12 to 1? 
+
+  
+}
